7b698a501b8f0e7ea5c1c46dfe9513ce095ad198
Amon <sergpolevoi@yandex.ru>
2024-06-12 18:25:07 +0400

    Заказы вроде норм

---
+++ api/services/order_service.py
@@ -0,0 +1,135 @@
+from collections import defaultdict
+from datetime import datetime, date
+
+from api.models import Order, Table, User, Product, OrderProduct, ProductIngredient
+
+
+class OrderService:
+    def change_order_status(self, order: Order, status: str):
+        if order.status == 'Обслужен':
+            order.table.client = None
+
+        order.status = status
+        order.save()
+
+    def cancel_order(self, order: Order) -> bool:
+        order.status = 'Отменён'
+
+    def create_order(  # TODO ПРОДУКТЫ ЕЩЁ
+            self,
+            client: User,
+            table: Table | None,
+            address: str | None,
+            products: list[tuple[Product, int]]
+    ) -> Order:
+        if not (table or address):
+            raise ValueError('Необходимо указать хотя бы один из аргументов: стол или адрес')
+
+        if table.client:
+            raise ValueError('Этот столик уже занят')
+
+        if table.waiter:
+            raise ValueError('Этот столик не обслуживает ни один официант')
+
+        table.client = client
+        table.save()
+
+        order = Order(status='Принят', date=datetime.now(), client=client, table=table, address=address)
+
+        for product, count in products:
+            for consumption in ProductIngredient.objects.filter(product=product):
+                ingredients_consumption = consumption.count * count
+
+                if consumption.ingredient.count < ingredients_consumption:
+                    raise ValueError(f'Недостаточно продуктов на складе для: {product.name}')
+
+                consumption.ingredient.save()
+
+        order.save()
+
+        return order
+
+    def get_orders(self, with_status: str | None, with_tables: list[int]) -> list[Order]:
+        orders = Order.objects
+
+        if with_status:
+            orders = orders.filter(status=with_status)
+
+        if with_tables:
+            orders = orders.filter(table__id__in=with_tables)
+
+        return list(filter(lambda o: o.products.exists(), orders))
+
+    def book_table(self, client: User, time: datetime, table: Table):
+        if any(
+            list(
+                map(
+                    lambda o: not o.products.exists(),
+                    Order.objects.filter(date=time, table=table).exclude(status='Отменён')
+                )
+            ) + list(
+                map(
+                    lambda o: not o.products.exists(),
+                    Order.objects.filter(date__gte=time, client_id=client.id).exclude(status='Отменён')
+                )
+            )
+        ):
+            raise ValueError('Столик уже забронирован')
+
+        order = Order(status='Принят', date=time, client=client, table=table)
+        order.save()
+
+    def get_all_bookings(self) -> list[Order]:
+        result = []
+
+        for order in Order.objects.all():
+            if not order.products.exists():
+                result.append(order)
+
+        return result
+
+    def get_available_tables(self, time: date) -> list[Table]:
+        result = []
+
+        current_date = datetime.today()
+
+        tables = Table.objects.all()
+
+        if current_date == date:
+            tables = tables.filter(client=None)
+
+        booked_tables = list(
+            map(
+                lambda order: order.table.id,
+                filter(
+                    lambda o: not o.products.exists(),
+                    Order.objects.filter(date=time)
+                )
+            )
+        )
+
+        for table in tables:
+            if table.id not in booked_tables:
+                result.append(table)
+
+        return result
+
+    def get_user_booking(self, client: User) -> Order | None:
+        orders = Order.objects.filter(client=client, date__gte=date.today()).exclude(status='Отменён')
+        orders = filter(lambda order: not order.products.exists(), orders)
+
+        return next(iter(orders), None)
+
+    def get_order(self, order_id: int) -> Order:
+        return Order.objects.get(id=order_id)
+
+    def analyze_sales(self, from_date: date, to_date: date) -> dict[Product, int]:
+        orders = Order.objects.filter(date__gte=from_date).filter(date__lte=to_date)
+
+        result = defaultdict(int)
+
+        for order in orders:
+            for position in OrderProduct.objects.filter(order=order):
+                result[position] += position.count
+
+        return result
